c3290fd6f1b13bd4e7dec91134f778135eabb340
Madeleine Griffin <madeleinecgriffin@gmail.com>
2017-12-10 22:53:23 -0600

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,32 @@
 //starting character stats
 
-	var ray = {
-		name: "ray",
+	var rey = {
+		name: "rey",
 		HP: 125,
 		attack: 13,
-		counterAttack: 15,
+		counterAttack: 16,
+		//win if battle kylo or finn first, lose if battle bb8 first
 	}
 	var kylo = {
 		name: "kylo",
 		HP: 140,
-		attack: 11,
-		counterAttack: 11,
+		attack: 12,
+		counterAttack: 10,
+		//win if battle finn first, lose if battle bb8 or rey first
 	}
 	var finn = {
 		name: "finn",
 		HP: 130,
 		attack: 12,
-		counterAttack: 16,
+		counterAttack: 14,
+		//win if battle kylo first, lose if battle bb8 or rey first
 	}
 	var bb8 = {
 		name: "bb8",
-		HP: 110,
+		HP: 105,
 		attack: 14,
 		counterAttack: 18,
+		//lose if battle rey first, win if battle kylo or finn first
 	}
 
 var turnCounter = 1;
@@ -30,28 +34,34 @@ var myCharacter;
 var myOpponent;
 var newAttack;
 var opponentChosen = false;
+var opponentCounter = 0;
 
 //choosing your character and the defenders - this moves the correct image to the correct location
-$("#ray").on("click", function () {
-	if($("#ray").parents("#characterList").length == 1) {
+$("#rey").on("click", function () {
+	if($("#rey").parents("#characterList").length == 1) {
 		$("#myCharacter").text("");
 		$("#myOpponent").text("");
-		$("#ray").detach().appendTo("#myCharacter");
+		$("#rey").detach().appendTo("#myCharacter");
 		$("#kylo").detach().appendTo("#opponentList");
 		$("#finn").detach().appendTo("#opponentList");
 		$("#bb8").detach().appendTo("#opponentList");
-		myCharacter = ray;
-		$("#myHealth").text(myCharacter.HP);
+		$("#bb8").addClass("opponent");
+		$("#finn").addClass("opponent");
+		$("#kylo").addClass("opponent");
+		myCharacter = rey;
+		$("#myHealth").text("HP: " + myCharacter.HP);
 		$("#myHealth").addClass("character");
+		$("#holdOpponentList").text("Now, click on one of the characters below to battle them.");
 	}
 
-	if($("#ray").parents("#opponentList").length == 1 && $("#myOpponent").children().length == 0) {
+	if($("#rey").parents("#opponentList").length == 1 && $("#myOpponent").children().length == 0) {
 		$("#myOpponent").text("");
-		$("#ray").detach().appendTo("#myOpponent");
-		myOpponent = ray;
+		$("#rey").detach().appendTo("#myOpponent");
+		myOpponent = rey;
 		opponentChosen = true;
-		$("#opponentHealth").text(myOpponent.HP);
+		$("#opponentHealth").text("HP: " + myOpponent.HP);
 		$("#opponentHealth").addClass("character");
+		$("#holdOpponentList").text("");
 	}
 })
 
@@ -59,13 +69,17 @@ $("#kylo").on("click", function () {
 	if($("#kylo").parents("#characterList").length == 1) {
 		$("#myCharacter").text("");
 		$("#myOpponent").text("");
-		$("#ray").detach().appendTo("#opponentList");
+		$("#rey").detach().appendTo("#opponentList");
 		$("#kylo").detach().appendTo("#myCharacter");
 		$("#finn").detach().appendTo("#opponentList");
 		$("#bb8").detach().appendTo("#opponentList");
+		$("#bb8").addClass("opponent");
+		$("#finn").addClass("opponent");
+		$("#rey").addClass("opponent");
 		myCharacter = kylo;
-		$("#myHealth").text(myCharacter.HP);
+		$("#myHealth").text("HP: " + myCharacter.HP);
 		$("#myHealth").addClass("character");
+		$("#holdOpponentList").text("Now, click on one of the characters below to battle them.");
 	}
 
 	if($("#kylo").parents("#opponentList").length == 1 && $("#myOpponent").children().length == 0) {
@@ -73,8 +87,9 @@ $("#kylo").on("click", function () {
 		$("#kylo").detach().appendTo("#myOpponent");
 		myOpponent = kylo;
 		opponentChosen = true;
-		$("#opponentHealth").text(myOpponent.HP);
+		$("#opponentHealth").text("HP: " + myOpponent.HP);
 		$("#opponentHealth").addClass("character");
+		$("#holdOpponentList").text("");
 	}
 })
 
@@ -82,13 +97,17 @@ $("#finn").on("click", function () {
 	if($("#finn").parents("#characterList").length == 1) {
 		$("#myCharacter").text("");
 		$("#myOpponent").text("");
-		$("#ray").detach().appendTo("#opponentList");
+		$("#rey").detach().appendTo("#opponentList");
 		$("#kylo").detach().appendTo("#opponentList");
 		$("#finn").detach().appendTo("#myCharacter");
 		$("#bb8").detach().appendTo("#opponentList");
+		$("#bb8").addClass("opponent");
+		$("#kylo").addClass("opponent");
+		$("#rey").addClass("opponent");
 		myCharacter = finn;
-		$("#myHealth").text(myCharacter.HP);
+		$("#myHealth").text("HP: " + myCharacter.HP);
 		$("#myHealth").addClass("character");
+		$("#holdOpponentList").text("Now, click on one of the characters below to battle them.");
 	}
 
 	if($("#finn").parents("#opponentList").length == 1 && $("#myOpponent").children().length == 0) {
@@ -96,8 +115,9 @@ $("#finn").on("click", function () {
 		$("#finn").detach().appendTo("#myOpponent");
 		myOpponent = finn;
 		opponentChosen = true;
-		$("#opponentHealth").text(myOpponent.HP);
+		$("#opponentHealth").text("HP: " + myOpponent.HP);
 		$("#opponentHealth").addClass("character");
+		$("#holdOpponentList").text("");
 	}
 })
 
@@ -105,13 +125,17 @@ $("#bb8").on("click", function () {
 	if($("#bb8").parents("#characterList").length == 1) {
 		$("#myCharacter").text("");
 		$("#myOpponent").text("");
-		$("#ray").detach().appendTo("#opponentList");
+		$("#rey").detach().appendTo("#opponentList");
 		$("#kylo").detach().appendTo("#opponentList");
 		$("#finn").detach().appendTo("#opponentList");
 		$("#bb8").detach().appendTo("#myCharacter");
+		$("#kylo").addClass("opponent");
+		$("#finn").addClass("opponent");
+		$("#rey").addClass("opponent");
 		myCharacter = bb8;
-		$("#myHealth").text(myCharacter.HP);
+		$("#myHealth").text("HP: " + myCharacter.HP);
 		$("#myHealth").addClass("character");
+		$("#holdOpponentList").text("Now, click on one of the characters below to battle them.");
 	}
 
 	if($("#bb8").parents("#opponentList").length == 1 && $("#myOpponent").children().length == 0) {
@@ -119,22 +143,20 @@ $("#bb8").on("click", function () {
 		$("#bb8").detach().appendTo("#myOpponent");
 		myOpponent = bb8;
 		opponentChosen = true;
-		$("#opponentHealth").text(myOpponent.HP);
+		$("#opponentHealth").text("HP: " + myOpponent.HP);
 		$("#opponentHealth").addClass("character");
+		$("#holdOpponentList").text("");
 	}
 })
 
 $(".attack").on("click", function () {
-	if($("#myCharacter").children().length == 0) {
-		$("#myCharacter").text("Please pick a character.");
-	}
-
-	if($("#myOpponent").children().length == 0 && $("#myCharacter").children().length == 1) {
-		$("#myOpponent").text("Please pick an opponent.");
+	if($("#myOpponent").children().length == 0) {
+		$("#fightText").prepend("Please pick a character and an opponent to begin.<br>");
 	}
 
 	if(opponentChosen === true) {
 		attackFunction(myCharacter, myOpponent);
+		console.log(opponentCounter);
 	}	
 })
 
@@ -144,17 +166,17 @@ function attackFunction(myCharacter, myOpponent) {
 		myCharacter.HP = myCharacter.HP - myOpponent.counterAttack;
 		myOpponent.HP = myOpponent.HP - newAttack;
 		$("#fightText").prepend(myCharacter.name + " did " + newAttack + " points of damage.  " + myOpponent.name + " did " + myOpponent.counterAttack + " points of damage.<br>");
-		$("#myHealth").text(myCharacter.HP);
-		$("#opponentHealth").text(myOpponent.HP);
+		$("#myHealth").text("HP: " + myCharacter.HP);
+		$("#opponentHealth").text("HP: " + myOpponent.HP);
 		turnCounter++;
 	}
 
-	if (myCharacter.HP > 0 && myOpponent.HP <= 0 && $("#opponentList").children().length == 0) {
-		$("#fightText").prepend("You win! All enemies defeated.<br>");
+	if (myCharacter.HP > 0 && myOpponent.HP <= 0 && opponentCounter == 2) {
+		$("#fightText").prepend("Congrats! You have defeated all of your foes. Refresh the page to play again.<br>");
 		$("#myOpponent").text("");
 		$("#opponentHealth").text("");
 		$("#opponentHealth").removeClass("character");
-		opponentChosen = false;
+		return
 	}
 
 	if (myCharacter.HP > 0 && myOpponent.HP <= 0) {
@@ -162,16 +184,12 @@ function attackFunction(myCharacter, myOpponent) {
 		$("#myOpponent").text("");
 		$("#opponentHealth").text("");
 		$("#opponentHealth").removeClass("character");
-		opponentChosen = false;
-	}
-
-	if (myCharacter.HP <= 0 && myOpponent.HP > 0) {
-		$("#fightText").prepend("You lose! Refresh the page to play again.<br>");
+		opponentCounter++;
 		opponentChosen = false;
 		return
 	}
 
-	if (myCharacter.HP <= 0 && myOpponent.HP <= 0) {
+	if (myCharacter.HP <= 0) {
 		$("#fightText").prepend("You lose! Refresh the page to play again.<br>");
 		opponentChosen = false;
 		return
